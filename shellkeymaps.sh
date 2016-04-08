6110ecc7430ff1d1d7f9018f8cb0b946e6e1eb25
Piero de Salvia <piero@localhost.localdomain>
2016-04-08 10:23:14 +0200

    shellkeymaps.sh

---
+++ shellkeymaps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bind '"\ek":backward-char'
+bind '"\el":forward-char'
+bind '"\ej":beginning-of-line'
+bind '"\e;":end-of-line'
+bind '"\eK":backward-word'
+bind '"\eL":forward-word'
+
